42e97eab83222ef37a491b5949be4ed1e2365add
Your Name <you@example.com>
2021-07-29 13:29:16 +0500

    HONEY add JavaScript

---
+++ index.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    let bar = document.querySelector('.fa-bars');
+
+    bar.addEventListener('click', function(){
+        document.querySelector('nav > ul').classList.toggle('active');
+    })
+
+    
+
+    window.addEventListener('scroll',function() {
+        let navigation = document.querySelector('nav');
+        navigation.classList.toggle('stick',window.scrollY > 0);
+    })
+    window.addEventListener('scroll', function () {
+        let button = document.querySelector('button');
+        button.classList.toggle('scrolBtn',window.scrollY > 500);
+    })
+    button.addEventListener('click', () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+
+
+
+})
